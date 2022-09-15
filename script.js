b29598809f28b54f6267948b30bd627aa4e702df
paneryk <eryk.wojdyla@gmail.com>
2022-09-15 21:08:57 +0200

    Completed a beta version, want to add more functions like a reset button, better inferface and real time score on the side of the consoleLog

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
 const weapons = ['rock', 'scissors', 'paper'];
+let i = 0;
+let userPoints = 0;
+let computerPoints = 0; 
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "draw";
-    }
-    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return "player";
-    }
-    else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return "computer";
-    }
-    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return "player";
-    }
-    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return "computer";
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return "player";
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return "computer";
-    }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => game(button.id));
+})
+if (userPoints >= 5 || userPoints >= 5) {
+    buttons.forEach((button) => {button.disabled = true;})
 }
 
-function game() {
+function playRound(playerSelection, computerSelection) {
 
-    let userPoints = 0;
-    let computerPoints = 0; 
+    if (playerSelection == computerSelection) return "draw";
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') return "player";
+    else if (playerSelection == 'rock' && computerSelection == 'paper') return "computer";
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') return "player";
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') return "computer";
+    else if (playerSelection == 'paper' && computerSelection == 'rock') return "player";
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') return "computer";
+}
 
-    for (let i=0; i < 5; i++) {
-        let getComputerChoice = (array) => array[Math.floor(Math.random()*array.length)];
-        let computerChoice = getComputerChoice(weapons);
-        let userChoice = (prompt("Choose your weapon: rock, scissors or paper:")).toLowerCase();
+function game(evt) {
+    i += 1;
+    console.log(i);
 
-        let result = playRound(userChoice, computerChoice);
+
+    let getComputerChoice = (array) => array[Math.floor(Math.random()*array.length)];
+    let computerSelection = getComputerChoice(weapons);
+    let playerSelection = evt;
+    let result = playRound(playerSelection, computerSelection);
+    const div = document.querySelector('#result');
+    const consoleLog = document.createElement('div');
+    const score = document.querySelector('#score');
+    consoleLog.classList.add('consoleLog');
 
         if (result == "player") {
-            console.log("The winner of round no "+(i+1)+" is PLAYER! He played "+userChoice+" agains computer's "+computerChoice);
             userPoints = userPoints+1;
-            console.log("Total score: player - "+userPoints+", computer - "+computerPoints);
+            consoleLog.textContent = "The winner of round no  is PLAYER! He played "+playerSelection+" agains computer's "+computerSelection;
+            score.textContent = "Score: player - "+userPoints+", computer - "+computerPoints;
         }
         else if (result == "computer") {
-            console.log("The winner of round no "+(i+1)+" is COMPUTER! It played "+computerChoice+" agains player's "+userChoice);
             computerPoints = computerPoints+1;
-            console.log("Total score: player - "+userPoints+", computer - "+computerPoints);
+            consoleLog.textContent = "The winner of round no  is COMPUTER! It played "+computerSelection+" agains player's "+playerSelection;
+            score.textContent = "Total score: player - "+userPoints+", computer - "+computerPoints;
         }
         else if (result == "draw") {
-            console.log("We have a DRAW in round no "+(i+1)+"! Computer and player both played "+computerChoice);
-            console.log("Total score: player - "+userPoints+", computer - "+computerPoints);
+            consoleLog.textContent = "We have a DRAW in round no! Computer and player both played "+computerSelection;
+            score.textContent = "Total score: player - "+userPoints+", computer - "+computerPoints;
         }
+    
+    div.appendChild(consoleLog);
+
+    if (userPoints>=5 || computerPoints >= 5) {
+        consoleLog.textContent = "END OF THE GAME";
+        div.appendChild(consoleLog);
+        buttons.forEach((button) => {button.disabled = true;})
     }
-if (computerPoints > userPoints) {
+    let gameStatus = [userPoints, computerPoints]
+    return gameStatus;
+    
+    } 
+/*if (computerPoints > userPoints) {
     console.log("The winner of 5 turn game is a COMPUTER! It has won by "+(computerPoints-userPoints)+" points.");
 }
 else if (userPoints > computerPoints) {
@@ -63,4 +75,4 @@ else {
 }
 
 game();
-
+*/
